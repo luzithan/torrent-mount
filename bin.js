a97ef02fed16c1dbefa552ea032677ae130ff235
matthieu <luzithan@gmail.com>
2017-10-13 09:51:24 +0200

    cleanup code

--- bin.js
+++ bin.js
@@ -1,81 +1,81 @@
 #!/usr/bin/env node
 
-var drive = require('./')
-var readTorrent = require('read-torrent')
-var log = require('single-line-log').stdout
-var prettysize = require('prettysize')
-var fuse = require('fuse-bindings')
-var fs = require('fs')
-var minimist = require('minimist')
+var drive = require('./');
+var readTorrent = require('read-torrent');
+var log = require('single-line-log').stdout;
+var prettysize = require('prettysize');
+var fuse = require('fuse-bindings');
+var fs = require('fs');
+var minimist = require('minimist');
 
 var argv = minimist(process.argv.slice(2), {
   alias: {mount: 'm', lazy: 'l'},
   boolean: ['lazy']
-})
+});
 
 if (!argv._[0]) {
-  console.error('Usage: torrent-mount torrent_or_magnet_link [directory]')
-  console.error()
-  console.error('  --mount,  -m   Mount location path [directory]')
-  console.error('  --lazy,   -l   Download files only if accessed')
-  console.error()
+  console.error('Usage: torrent-mount torrent_or_magnet_link [directory]');
+  console.error();
+  console.error('  --mount,  -m   Mount location path [directory]');
+  console.error('  --lazy,   -l   Download files only if accessed');
+  console.error();
   process.exit(1)
 }
 
 readTorrent(argv._[0], function (err, torrent, raw) {
   if (err) {
-    console.error(err.message)
+    console.error(err.message);
     process.exit(2)
   }
 
-  var mnt = fs.realpathSync(argv.mount || '.')
-  var isLazy = argv.lazy
-  var engine = drive(raw, mnt, isLazy)
-  var hs = 0
+  var mnt = fs.realpathSync(argv.mount || '.');
+  var isLazy = argv.lazy;
+  var engine = drive(raw, mnt, isLazy);
+  var hs = 0;
 
   engine.on('hotswap', function () {
     hs++
-  })
+  });
 
-  log('Initializing swarm and verifying data...\n')
+  log('Initializing swarm and verifying data...\n');
   engine.on('mount', function (mnt) {
-    log('Mounted ' + engine.files.length + ' files, ' + prettysize(engine.torrent.length) + ' in ' + engine.torrent.name)
-    log.clear()
+    log('Mounted ' + engine.files.length + ' files, ' + prettysize(engine.torrent.length) + ' in ' + engine.torrent.name);
+    log.clear();
 
     var notChoked = function (result, wire) {
       return result + (wire.peerChoking ? 0 : 1)
-    }
+    };
 
     var status = function () {
-      var down = prettysize(engine.swarm.downloaded)
-      var downSpeed = prettysize(engine.swarm.downloadSpeed()).replace('Bytes', 'b') + '/s'
-      var up = prettysize(engine.swarm.uploaded)
-      var upSpeed = prettysize(engine.swarm.uploadSpeed()).replace('Bytes', 'b') + '/s'
+      var down = prettysize(engine.swarm.downloaded);
+      var downSpeed = prettysize(engine.swarm.downloadSpeed()).replace('Bytes', 'b') + '/s';
+      var up = prettysize(engine.swarm.uploaded);
+      var upSpeed = prettysize(engine.swarm.uploadSpeed()).replace('Bytes', 'b') + '/s';
 
       log(
         'Connected to ' + engine.swarm.wires.reduce(notChoked, 0) + '/' + engine.swarm.wires.length + ' peers\n' +
         'Downloaded ' + down + ' (' + downSpeed + ') with ' + hs + ' hotswaps\n' +
         'Uploaded ' + up + ' (' + upSpeed + ')\n'
       )
-    }
+    };
 
-    var interval = setInterval(status, 500)
-    status()
+    var interval = setInterval(status, 500);
+    status();
 
     var exit = function () {
-      setTimeout(process.kill.bind(process, process.pid), 2000).unref()
-      process.removeListener('SIGINT', exit)
-      process.removeListener('SIGTERM', exit)
-      log('Shutting down...\n')
-      clearInterval(interval)
+      setTimeout(process.kill.bind(process, process.pid), 2000).unref();
+      process.removeListener('SIGINT', exit);
+      process.removeListener('SIGTERM', exit);
+      log('Shutting down...\n');
+      clearInterval(interval);
       fuse.unmount(mnt, function () {
         fs.rmdir(mnt, function () {
           process.exit()
         })
       })
-    }
+    };
 
-    process.on('SIGINT', exit)
+    process.on('SIGINT', exit);
     process.on('SIGTERM', exit)
   })
-})
+});
--- index.js
+++ index.js
@@ -1,124 +1,124 @@
-var fuse = require('fuse-bindings')
-var mkdirp = require('mkdirp')
-var torrents = require('torrent-stream')
-var path = require('path')
+var fuse = require('fuse-bindings');
+var mkdirp = require('mkdirp');
+var torrents = require('torrent-stream');
+var path = require('path');
 
-var ENOENT = -2
-var EPERM = -1
+var ENOENT = -2;
+var EPERM = -1;
 
 module.exports = function (source, mnt, isLazy) {
-  if (!mnt) mnt = '.'
+  if (!mnt) mnt = '.';
 
-  var handlers = {}
-  var engine = torrents(source)
-  var ctime = new Date()
-  var mtime = new Date()
+  var handlers = {};
+  var engine = torrents(source);
+  var ctime = new Date();
+  var mtime = new Date();
 
   engine.on('ready', function () {
     if (engine.torrent.name === (engine.files[0] && engine.files[0].path)) {
-      var dir = engine.torrent.name.replace(/\.[^.]+$/, '')
+      var dir = engine.torrent.name.replace(/\.[^.]+$/, '');
       engine.torrent.files.forEach(function (file) {
         file.path = path.join(dir, file.path)
-      })
+      });
       engine.torrent.name = dir
     }
 
-    mnt = path.join(mnt, path.resolve('/', engine.torrent.name))
+    mnt = path.join(mnt, path.resolve('/', engine.torrent.name));
     fuse.unmount(mnt, function () {
       mkdirp(mnt, function () {
-        fuse.mount(mnt, handlers)
+        fuse.mount(mnt, handlers);
         engine.emit('mount', mnt)
       })
-    })
+    });
 
     if (!isLazy) {
       engine.files.forEach(function (file) {
-        file.path = file.path.slice(engine.torrent.name.length + 1)
+        file.path = file.path.slice(engine.torrent.name.length + 1);
         file.select()
       })
     }
 
     engine.on('uninterested', function () {
       engine.swarm.pause()
-    })
+    });
 
     engine.on('interested', function () {
       engine.swarm.resume()
     })
-  })
+  });
 
   var find = function (path) {
     return engine.files.reduce(function (result, file) {
       return result || (file.path === path && file)
     }, null)
-  }
+  };
 
-  handlers.displayFolder = true
+  handlers.displayFolder = true;
 
   handlers.getattr = function (path, cb) {
-    path = path.slice(1)
+    path = path.slice(1);
 
-    var stat = {}
-    var file = find(path)
+    var stat = {};
+    var file = find(path);
 
-    stat.ctime = ctime
-    stat.mtime = mtime
-    stat.atime = new Date()
-    stat.uid = process.getuid()
-    stat.gid = process.getgid()
+    stat.ctime = ctime;
+    stat.mtime = mtime;
+    stat.atime = new Date();
+    stat.uid = process.getuid();
+    stat.gid = process.getgid();
 
     if (file) {
-      stat.size = file.length
-      stat.mode = 33206 // 0100666
+      stat.size = file.length;
+      stat.mode = 33206; // 0100666
       return cb(0, stat)
     }
 
-    stat.size = 4096
-    stat.mode = 16877 // 040755
+    stat.size = 4096;
+    stat.mode = 16877; // 040755
 
-    if (!path) return cb(0, stat)
+    if (!path) return cb(0, stat);
 
     var dir = engine.files.some(function (file) {
       return file.path.indexOf(path + '/') === 0
-    })
+    });
 
-    if (!dir) return cb(ENOENT)
+    if (!dir) return cb(ENOENT);
     cb(0, stat)
-  }
+  };
 
-  var files = {}
+  var files = {};
 
   handlers.open = function (path, flags, cb) {
-    path = path.slice(1)
+    path = path.slice(1);
 
-    var file = find(path)
-    if (!file) return cb(ENOENT)
+    var file = find(path);
+    if (!file) return cb(ENOENT);
 
-    var fs = files[path] = files[path] || []
-    var fd = fs.indexOf(null)
-    if (fd === -1) fd = fs.length
+    var fs = files[path] = files[path] || [];
+    var fd = fs.indexOf(null);
+    if (fd === -1) fd = fs.length;
 
-    fs[fd] = {offset: 0}
+    fs[fd] = {offset: 0};
 
     cb(0, fd)
-  }
+  };
 
   handlers.release = function (path, handle, cb) {
-    path = path.slice(1)
+    path = path.slice(1);
 
-    var fs = files[path] || []
-    var f = fs[handle]
+    var fs = files[path] || [];
+    var f = fs[handle];
 
-    if (f && f.stream) f.stream.destroy()
-    fs[handle] = null
+    if (f && f.stream) f.stream.destroy();
+    fs[handle] = null;
 
     cb(0)
-  }
+  };
 
   handlers.readdir = function (path, cb) {
-    path = path.slice(1)
+    path = path.slice(1);
 
-    var uniq = {}
+    var uniq = {};
     var files = engine.files
       .filter(function (file) {
         return file.path.indexOf(path ? path + '/' : '') === 0
@@ -127,78 +127,78 @@ module.exports = function (source, mnt, isLazy) {
         return file.path.slice(path ? path.length + 1 : 0).split('/')[0]
       })
       .filter(function (name) {
-        if (uniq[name]) return false
-        uniq[name] = true
+        if (uniq[name]) return false;
+        uniq[name] = true;
         return true
-      })
+      });
 
-    if (!files.length) return cb(ENOENT)
+    if (!files.length) return cb(ENOENT);
     cb(0, files)
-  }
+  };
 
   handlers.read = function (path, handle, buf, len, offset, cb) {
-    path = path.slice(1)
+    path = path.slice(1);
 
-    var file = find(path)
-    var fs = files[path] || []
-    var f = fs[handle]
+    var file = find(path);
+    var fs = files[path] || [];
+    var f = fs[handle];
 
-    if (!file) return cb(ENOENT)
-    if (!f) return cb(ENOENT)
+    if (!file) return cb(ENOENT);
+    if (!f) return cb(ENOENT);
 
-    if (len + offset > file.length) len = file.length - offset
+    if (len + offset > file.length) len = file.length - offset;
 
     if (f.stream && f.offset !== offset) {
-      f.stream.destroy()
+      f.stream.destroy();
       f.stream = null
     }
 
     if (!f.stream) {
-      f.stream = file.createReadStream({start: offset})
+      f.stream = file.createReadStream({start: offset});
       f.offset = offset
     }
 
     var loop = function () {
-      var result = f.stream.read(len)
-      if (!result) return f.stream.once('readable', loop)
-      result.copy(buf)
+      var result = f.stream.read(len);
+      if (!result) return f.stream.once('readable', loop);
+      result.copy(buf);
       cb(result.length)
-    }
+    };
 
     loop()
-  }
+  };
 
   handlers.write = function (path, handle, buf, len, offset, cb) {
     cb(EPERM)
-  }
+  };
 
   handlers.unlink = function (path, cb) {
     cb(EPERM)
-  }
+  };
 
   handlers.rename = function (src, dst, cb) {
     cb(EPERM)
-  }
+  };
 
   handlers.mkdir = function (path, mode, cb) {
     cb(EPERM)
-  }
+  };
 
   handlers.rmdir = function (path, cb) {
     cb(EPERM)
-  }
+  };
 
   handlers.create = function (path, mode, cb) {
     cb(EPERM)
-  }
+  };
 
   handlers.getxattr = function (path, name, buffer, length, offset, cb) {
     cb(EPERM)
-  }
+  };
 
   handlers.setxattr = function (path, name, buffer, length, offset, flags, cb) {
     cb(0)
-  }
+  };
 
   handlers.statfs = function (path, cb) {
     cb(0, {
@@ -214,11 +214,11 @@ module.exports = function (source, mnt, isLazy) {
       flag: 1000000,
       namemax: 1000000
     })
-  }
+  };
 
   handlers.destroy = function (cb) {
     cb()
-  }
+  };
 
   return engine
-}
+};
